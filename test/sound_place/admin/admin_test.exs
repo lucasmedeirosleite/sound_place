17849602984180931acfb7c8894631059741aff3
Lucas Medeiros <lucastoc@gmail.com>
2017-08-16 17:57:08 -0300

    create admin context and user

---
+++ test/sound_place/admin/admin_test.exs
@@ -0,0 +1,67 @@
+defmodule SoundPlace.AdminTest do
+  use SoundPlace.DataCase
+
+  alias SoundPlace.Admin
+
+  describe "users" do
+    alias SoundPlace.Admin.User
+
+    @valid_attrs %{name: "some name", username: "some username"}
+    @update_attrs %{name: "some updated name", username: "some updated username"}
+    @invalid_attrs %{name: nil, username: nil}
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Admin.create_user()
+
+      user
+    end
+
+    test "list_users/0 returns all users" do
+      user = user_fixture()
+      assert Admin.list_users() == [user]
+    end
+
+    test "get_user!/1 returns the user with given id" do
+      user = user_fixture()
+      assert Admin.get_user!(user.id) == user
+    end
+
+    test "create_user/1 with valid data creates a user" do
+      assert {:ok, %User{} = user} = Admin.create_user(@valid_attrs)
+      assert user.name == "some name"
+      assert user.username == "some username"
+    end
+
+    test "create_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Admin.create_user(@invalid_attrs)
+    end
+
+    test "update_user/2 with valid data updates the user" do
+      user = user_fixture()
+      assert {:ok, user} = Admin.update_user(user, @update_attrs)
+      assert %User{} = user
+      assert user.name == "some updated name"
+      assert user.username == "some updated username"
+    end
+
+    test "update_user/2 with invalid data returns error changeset" do
+      user = user_fixture()
+      assert {:error, %Ecto.Changeset{}} = Admin.update_user(user, @invalid_attrs)
+      assert user == Admin.get_user!(user.id)
+    end
+
+    test "delete_user/1 deletes the user" do
+      user = user_fixture()
+      assert {:ok, %User{}} = Admin.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn -> Admin.get_user!(user.id) end
+    end
+
+    test "change_user/1 returns a user changeset" do
+      user = user_fixture()
+      assert %Ecto.Changeset{} = Admin.change_user(user)
+    end
+  end
+end
