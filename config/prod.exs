e0e19122366068a9422da07e4237f65985de49d2
Lucas Medeiros <lucastoc@gmail.com>
2017-10-24 00:23:12 -0300

    add better docker usage

--- config/prod.exs
+++ config/prod.exs
@@ -1,57 +1,21 @@
 use Mix.Config
 
-config :sound_place,
+config :sound_place, 
        SoundPlaceWeb.Endpoint,
-       http: [port: {:system, "PORT"}],
-       url: [scheme: "https", host: "boiling-plateau-96706.herokuapp.com", port: 443],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]],
-       cache_static_manifest: "priv/static/cache_manifest.json",
        load_from_system_env: true,
+       http: [port: System.get_env("PORT")],
+       url: [host: System.get_env("HOST"), port: System.get_env("PORT")],
+       cache_static_manifest: "priv/static/cache_manifest.json",
        secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :sound_place,
        SoundPlace.Repo,
        adapter: Ecto.Adapters.Postgres,
-       url: System.get_env("DATABASE_URL"),
-       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-       ssl: true
+       hostname: System.get_env("SOUND_PLACE_DB_HOST"),
+       username: System.get_env("SOUND_PLACE_DB_USERNAME"),
+       password: System.get_env("SOUND_PLACE_DB_PASSWORD"),
+       database: "sound_place_production",
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+       ssl: false
 
 config :logger, level: :info
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :sound_place, SoundPlaceWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [:inet6,
-#               port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :sound_place, SoundPlaceWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :sound_place, SoundPlaceWeb.Endpoint, server: true
-#
